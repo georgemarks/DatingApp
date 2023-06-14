4693b7c907da1c05b2e21ff23e846a4b7c3dd55a
Marks, George <George.Marks@dgs.ca.gov>
2023-06-14 06:59:54 -0700

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Configure the HTTP request pipeline.
+//app.UseAuthorization();
+
+var app =builder.Build();
+
+app.MapControllers();
+
+app.Run();
