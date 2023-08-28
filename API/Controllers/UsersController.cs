4ef44e551cf41e1622b4d5040da0fb19314e3ca7
Marks, George <George.Marks@dgs.ca.gov>
2023-08-28 15:16:14 -0700

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,14 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")] // GET /api/users
-
-    public class UsersController : ControllerBase
+{  
+    [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -16,6 +15,7 @@ namespace API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -32,35 +32,3 @@ namespace API.Controllers
         }
     }
 }
-
-
-// namespace API.Controllers
-// {
-//     [ApiController]
-//     [Route("api/[controller]")]     // /api/users
-    
-//     public class UsersController : ControllerBase
-//     {
-       
-//         private readonly DataContext _context;
-
-//         public UsersController(DataContext context)
-//         {
-//             _context = context;
-//         }
-
-//         [HttpGet]
-//         public ActionResult<IEnumerable<AppUser>> GetUsers()
-//         {
-//             var users = _context.Users.ToList();
-            
-//             return users;
-//         }
-        
-//         [HttpGet("{id}")]
-//         public ActionResult<AppUser> GetUser(int id)
-//         {
-//             return _context.Users.Find(id);
-//         }
-//     }
-// }
